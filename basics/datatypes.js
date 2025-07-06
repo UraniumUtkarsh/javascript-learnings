cad54637a68725d06833d711f041a8a5c66ca78c
Utkarsh Pandey <35457627+UraniumUtkarsh@users.noreply.github.com>
2025-07-06 17:28:04 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"new strict"; //treated as newer version of Javascript
+
+// alert(3+3) //We are using node js , not browser
+// console.log(3+3);
+// console.log("Utkarsh");
+
+let name = "Utkarsh"
+let age = 22
+let isLoggedIn = true
+
+/* NOTES on datatypes
+number ---- 2 to power 53
+bigint
+string "string"
+boolean -> true/false
+null -> standalone value
+undefined -> not yet defined
+symbol -> unique
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
+
