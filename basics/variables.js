736d0950237c7504a2ebd9b16fd45d485c993839
Utkarsh Pandey <35457627+UraniumUtkarsh@users.noreply.github.com>
2025-07-06 16:56:21 +0000

    added variables.js

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail = "utkarsh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+
+// accountId=144554 not allowed
+accountEmail = "naman@gmail.com"
+accountPassword="6789"
+accountCity="Ballia"
+let accountState
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
